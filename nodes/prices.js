8c46fa74192d357a08048b9669c0fff80e573d88
aikitori <aikitori@guthub.com>
2024-05-31 08:49:32 +0200

    add prices

---
+++ nodes/prices.js
@@ -0,0 +1,32 @@
+module.exports = function(RED) {
+    const axios = require('axios')
+    function PricesNode(config) {
+        RED.nodes.createNode(this,config);
+        this.bzn = config.bzn.toUpperCase() ?? 'DE-LU' ;
+        this.start = config.start;
+        this.end = config.end;
+        var node = this;
+        var headers = {
+            accept: 'application/json'
+        }
+        var options = {
+            bzn: node.bzn,
+            start: node.start,
+            end: node.end
+        }
+        node.on('input', function(msg) {
+            axios.get('https://api.energy-charts.info/price',
+            { params: options, headers }).then(function (response) {
+            let prices = response.data
+            msg.payload = prices
+            node.send(msg)
+          })
+            .catch(error => {
+              node.warn({ error: error.message,params: options })
+            })
+        }
+    )}
+    RED.nodes.registerType("prices",PricesNode);
+} 
+
+
